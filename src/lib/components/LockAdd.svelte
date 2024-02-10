b338e925fe7910879d7f24906b275f09b61cbb3c
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-10 18:29:58 -0500

    Move code into a LockAdd component

---
+++ src/lib/components/LockAdd.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    let lockerCode: number[];
+    function generateCode() {
+        let newDigits: number[] = [];
+        for(let i = 0; i < 4; i++) {
+            newDigits.push(Math.floor(Math.random() * 9));
+        }
+        lockerCode = [...newDigits];
+    }
+</script>
+
+<style>
+    .black-border {
+        border: 2px solid black;
+        padding: 10px;
+    }
+    .textarea {
+        height: 5rem;
+        width: 50%;
+    }
+</style>
+
+<div class="black-border">
+<h2>Add a lock</h2>
+<div>
+    <button on:click={() => generateCode()}>Generate a code</button>
+    <button on:click={() => {}}>Save</button>
+</div>
+<p>Your locker code is: {lockerCode ? lockerCode : "..."}</p>
+<p>Notes:</p>
+<textarea class="textarea"></textarea>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,5 @@
-<script lang="ts">
-    let lockerCode: number[];
-    function generateCode() {
-        let newDigits: number[] = [];
-        for(let i = 0; i < 4; i++) {
-            newDigits.push(Math.floor(Math.random() * 9));
-        }
-        lockerCode = [...newDigits];
-    }
+<script>
+	import LockAdd from "$lib/components/LockAdd.svelte";
 </script>
 <h1>Lockify</h1>
-<button on:click={() => generateCode()}>Generate a code</button>
-<p>Your locker code is: {lockerCode ? lockerCode : "..."}</p>
-<p>Notes:</p>
-<textarea></textarea>
+<LockAdd />
