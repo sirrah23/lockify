9bc9304e7722f81b35abe51ff3f6c8dc844b7569
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-11 00:51:16 -0500

    Revert "Add background color"
    This reverts commit 87a8a7739cc5d9df63bb6938f370d9a6ffe1fec6.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,25 +6,20 @@
 	let showLockAddModal = false;
 </script>
 
-<div class="has-background-white-ter">
-	<section class="hero is-info">
-		<div class="hero-body">
-			<p class="title is-1">Lockify</p>
-			<p class="subtitle">Protect the goods</p>
-		</div>
-	</section>
-	<div class="section">
-		<button
-			class="button is-info is-medium is-pulled-right"
-			on:click={() => (showLockAddModal = true)}>Add</button
-		>
+<section class="hero is-info">
+	<div class="hero-body">
+		<p class="title is-1">Lockify</p>
+		<p class="subtitle">Protect the goods</p>
 	</div>
-	<div class="section">
-		<LockList />
-	</div>
-	{#if showLockAddModal}
-		<div class="section">
-			<LockAdd onDone={() => (showLockAddModal = false)} />
-		</div>
-	{/if}
+</section>
+<div class="section">
+	<button class="button is-info is-medium is-pulled-right" on:click={() => showLockAddModal = true}>Add</button>
 </div>
+<div class="section">
+	<LockList />
+</div>
+{#if showLockAddModal}
+	<div class="section">
+		<LockAdd onDone={() => (showLockAddModal = false)} />
+	</div>
+{/if}
