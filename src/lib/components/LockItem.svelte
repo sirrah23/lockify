6cf4184cfffc0993c76c4218bd4aeb220433bee9
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-10 20:46:29 -0500

    Move lock items into their own component

---
+++ src/lib/components/LockItem.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import type { Lock } from "$lib/data/db";
+    export let lock: Lock
+</script>
+
+<style>
+    .black-border {
+        border: 2px solid black;
+        padding: 10px;
+    }
+    .container {
+        padding: 10px;
+        margin: 10px;
+    }
+    .textarea {
+        height: 5rem;
+        width: 50%;
+    }
+</style>
+
+<div 
+    class="container black-border"
+>
+    <p>Code: {lock.lock}</p>
+    <p>Created at: {lock.createdAt}</p>
+    <p>Note:</p>
+    <textarea class="textarea" readonly disabled>{lock.note || ""}</textarea>
+</div>
--- src/lib/components/LockList.svelte
+++ src/lib/components/LockList.svelte
@@ -2,40 +2,21 @@
     import { liveQuery } from "dexie";
     import { db } from "$lib/data/db";
 	import { flip } from "svelte/animate";
+	import LockItem from "./LockItem.svelte";
 
     let locks = liveQuery(
         () => db.locks.orderBy('createdAt').reverse().toArray()
     );
 </script>
 
-<style>
-    .black-border {
-        border: 2px solid black;
-        padding: 10px;
-    }
-    .container {
-        padding: 10px;
-        margin: 10px;
-    }
-    .textarea {
-        height: 5rem;
-        width: 50%;
-    }
-</style>
-
 <div >
     <h2>Lock list</h2>
     {#if $locks}
         {#each $locks as lock (lock.id)}
-            <!-- Consider a lock component here with code hiding -->
             <div 
-                class="container black-border"
                 animate:flip={{duration: 500}}
             >
-                <p>Code: {lock.lock}</p>
-                <p>Created at: {lock.createdAt}</p>
-                <p>Note:</p>
-                <textarea class="textarea" readonly disabled>{lock.note || ""}</textarea>
+                <LockItem lock={lock} />
             </div>
         {/each}
     {:else}
