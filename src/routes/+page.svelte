87a8a7739cc5d9df63bb6938f370d9a6ffe1fec6
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-11 00:46:58 -0500

    Add background color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,20 +6,25 @@
 	let showLockAddModal = false;
 </script>
 
-<section class="hero is-info">
-	<div class="hero-body">
-		<p class="title is-1">Lockify</p>
-		<p class="subtitle">Protect the goods</p>
-	</div>
-</section>
-<div class="section">
-	<button class="button is-info is-medium is-pulled-right" on:click={() => showLockAddModal = true}>Add</button>
-</div>
-<div class="section">
-	<LockList />
-</div>
-{#if showLockAddModal}
+<div class="has-background-white-ter">
+	<section class="hero is-info">
+		<div class="hero-body">
+			<p class="title is-1">Lockify</p>
+			<p class="subtitle">Protect the goods</p>
+		</div>
+	</section>
 	<div class="section">
-		<LockAdd onDone={() => (showLockAddModal = false)} />
+		<button
+			class="button is-info is-medium is-pulled-right"
+			on:click={() => (showLockAddModal = true)}>Add</button
+		>
 	</div>
-{/if}
+	<div class="section">
+		<LockList />
+	</div>
+	{#if showLockAddModal}
+		<div class="section">
+			<LockAdd onDone={() => (showLockAddModal = false)} />
+		</div>
+	{/if}
+</div>
