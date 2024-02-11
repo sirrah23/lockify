54c326f0f124b343d6b402dfba678f2841af8c27
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-11 17:04:05 -0500

    Add link to Github

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import 'bulma/css/bulma.css';
+	import githubLogo from '$lib/assets/github-mark.png';
 	import LockAdd from '$lib/components/LockAdd.svelte';
 	import LockList from '$lib/components/LockList.svelte';
 
@@ -13,6 +14,11 @@
 	</div>
 </section>
 <div class="section">
+	<a href="https://github.com/sirrah23/lockify">
+		<figure class="image is-48x48 is-pulled-right ml-2">
+			<img class="is-rounded" alt="link to github" src={githubLogo}>
+		</figure>
+	</a>
 	<button
 		class="button is-info is-medium is-pulled-right"
 		on:click={() => (showLockAddModal = true)}>Add</button
