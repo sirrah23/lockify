2210795295c5420fcad0b14df4ca2d3384a02c86
sirrah23 <sirrah23@users.noreply.github.com>
2024-02-11 19:04:08 -0500

    Add initial load animation ✨

--- src/lib/components/LockList.svelte
+++ src/lib/components/LockList.svelte
@@ -1,16 +1,31 @@
 <script lang="ts">
-	import { liveQuery } from 'dexie';
-	import { db } from '$lib/data/db';
+	import { liveQuery, type Subscription } from 'dexie';
+	import { db, type Lock } from '$lib/data/db';
 	import { flip } from 'svelte/animate';
 	import LockItem from './LockItem.svelte';
+	import { onDestroy, onMount } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-	let locks = liveQuery(() => db.locks.orderBy('createdAt').reverse().toArray());
+	let locks: Lock[] = [];
+
+	let subscription: Subscription;
+	onMount(() => {
+		subscription = liveQuery(() => db.locks.orderBy('createdAt').reverse().toArray()).subscribe(
+			(newLocks) => {
+				locks = newLocks;
+			}
+		);
+	});
+
+	onDestroy(() => {
+		subscription && subscription.unsubscribe();
+	});
 </script>
 
 <div>
-	{#if $locks}
-		{#each $locks as lock (lock.id)}
-			<div animate:flip={{ duration: 500 }} class="mb-2">
+	{#if locks}
+		{#each locks as lock (lock.id)}
+			<div transition:fade={{ duration: 500 }} animate:flip={{ duration: 500 }} class="mb-2">
 				<LockItem {lock} />
 			</div>
 		{/each}
